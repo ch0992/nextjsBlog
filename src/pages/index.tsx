12f21ffd53cd7270f60ef1d1ed08ce173b94359a
최영규 <yeonggyu-choi@choeyeocBookPro.home>
2020-12-28 14:53:33 -1000

    first change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, This is YGtoken's Blog<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">choilee6@naver.com</span>
+          <h2>YGToken's Blog</h2>
           <SocialList />
         </div>
       </div>
